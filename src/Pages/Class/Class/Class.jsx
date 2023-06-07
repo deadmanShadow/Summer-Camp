1f0ef5ac67c4ee8c4a51686bbe5afa017dacc242
anUnnecessary <raihanshamil33@gmail.com>
2023-06-07 22:30:59 +0600

    image and menuitems added

--- src/Pages/Class/Class/Class.jsx
+++ src/Pages/Class/Class/Class.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../../Shared/Cover/Cover';
+import useMenu from '../../../Hooks/useMenu';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import ClassCategory from './ClassCategory/ClassCategory';
 
 
 const Class = () => {
+    const [menu] = useMenu();
+    const quran = menu.filter(item=>item.category === 'quran');
+    const fashion = menu.filter(item=>item.category === 'fashion');
+    const dance = menu.filter(item=>item.category === 'dance');
+    const drawing = menu.filter(item=>item.category === 'drawing');
+    const artscrafts = menu.filter(item=>item.category === 'artscrafts');
+    const psychics = menu.filter(item=>item.category === 'psychics');
     return (
         <div>
             <Helmet>
                 <title>Summer | Classes</title>
             </Helmet>
             <Cover></Cover>
+            <SectionTitle subHeading="Choose Your" heading="Favorite Courses"></SectionTitle>
+          <div className='mx-auto grid lg:grid-cols-2'>
+          <ClassCategory items={artscrafts}></ClassCategory>
+            <ClassCategory items={drawing}></ClassCategory>
+            <ClassCategory items={dance}></ClassCategory>
+            <ClassCategory items={quran}></ClassCategory>
+            <ClassCategory items={fashion}></ClassCategory>
+            <ClassCategory items={psychics}></ClassCategory>
+          </div>
         </div>
     );
 };
--- src/Shared/MenuItem/MenuItem.jsx
+++ src/Shared/MenuItem/MenuItem.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const MenuItem = ({ item }) => {
     const { image, name, seat, price } = item;
     return (
-        <div className='ms-4 flex space-x-4 mt-6'>
+        <div className='ms-4 space-x-4 mt-6'>
             <div>
-                <img className='w-[100px]' src={image} alt="" />
-                <h3 className='uppercase'>{name}-----------</h3>
+                <img className='w-[200px]' src={image} alt="" />
+                <h3 className='text-2xl uppercase mt-4'>{name}-------</h3>
                 <p>Seat Available: {seat}</p>
             </div>
-            <p className='text-red-500'>Price: ${price}</p>
+            <button className='text-red-500'>Price: ${price}</button>
         </div>
     );
 };
