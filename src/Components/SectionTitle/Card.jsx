9d09c0094ee8b3456266865b8f6ade257a0e602c
anUnnecessary <raihanshamil33@gmail.com>
2023-06-10 14:48:08 +0600

    items add cart added here

--- src/Components/SectionTitle/Card.jsx
+++ src/Components/SectionTitle/Card.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 const Card = ({item}) => {
     const { image, name, seat, price } = item;
+    const handleAddToCart = item => {
+        console.log(item);
+    }
     return (
         <div>
             <div className="card w-96 bg-base-100 shadow-xl">
@@ -10,7 +13,7 @@ const Card = ({item}) => {
                     <p>Available Seat: {seat}</p>
                     <p>Course Price: ${price}</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Add To Cart</button>
+                        <button onClick={()=> handleAddToCart(item)} className="btn btn-primary">Add To Cart</button>
                     </div>
                 </div>
             </div>
--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from "react-router-dom";
+import { AuthContext } from '../../Pages/Providers/AuthProvider';
+import { FaShoppingCart } from 'react-icons/fa';
 
 const NavBar = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.log(error));
+    }
     return (
         <div>
             <div className="navbar bg-base-100">
@@ -10,7 +19,7 @@ const NavBar = () => {
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">  
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         </ul>
                     </div>
                     <a className="btn btn-ghost normal-case text-xl">Summer School Camp</a>
@@ -20,11 +29,24 @@ const NavBar = () => {
                         <li><Link to='/'>Home</Link></li>
                         <li><Link to='/class'>Classes</Link></li>
                         <li><Link to='/instructors'>Instructors</Link></li>
-                        <li><Link>Dashboard</Link></li>
+                        <li><Link to="/dashboard">Dashboard</Link></li>
                         <li><Link to='/login'>Login</Link></li>
                         <li><Link to='/register'>Register</Link></li>
+                        <li> <Link to="/">
+                            <button className="btn btn-sm">
+                            <FaShoppingCart></FaShoppingCart>
+                                <div className="badge badge-secondary">+0</div></button>
+                        </Link></li>
                     </ul>
                 </div>
+                {
+                    user ? <>
+                        <span className='text-sm'>{user?.displayName}</span>
+                        <button onClick={handleLogOut} className='btn btn-ghost'>Logout</button>
+                    </> : <>
+                        <li><Link to="/login">Login</Link></li>
+                    </>
+                }
             </div>
         </div>
     );
